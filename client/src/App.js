93c6f26a2a10918b1618530e88c64de2c577cdac
Moha327 <81511588+Moha327@users.noreply.github.com>
2021-08-13 18:15:46 +0300

    hello

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import './App.css'
 import React from 'react'
-import { Router } from '@reach/router'
+import { Router,useState } from '@reach/router'
 import Chat from './chatfolder/Chat'
 import Origin from './Original'
 
@@ -13,7 +13,9 @@ import DetailAuthor from './authorFolder/Detail'
 import UpdateAuthor from './authorFolder/Update'
 
 import LalaChat from './chatfolder/LalaChat'
+import About from './association/About'
 
+import {FooterContainer} from './container/footer'
 function App() {
   const [name] = useState("asd")
 
@@ -30,10 +32,17 @@ function App() {
 
         <Chat path="/chat" />
         <LalaChat path="/lala" name={name} />
+
+        <About path = "/about" />
       </Router>
 
       <h3>End Main XxX</h3>
       <Origin />
+      return{
+        <>
+        <FooterContainer />
+        </>
+      }
     </div>
   );
 }
